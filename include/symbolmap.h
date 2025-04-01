32d20d1fd5a1e4bd5a53fd20c7c2e42e7c518ae0
msashank910 <msashank910@gmail.com>
2025-03-31 23:35:05 -0400

    Deleted stale comments in Cmakelists.txt. Made SymbolMap Class, with look up function. Need to incorporate in debugger class

---
+++ include/symbolmap.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <cstdint>
+
+#include <elf/elf++.hh>
+
+
+class SymbolMap {
+
+public:
+    SymbolMap();
+    SymbolMap(const elf::elf& elf);
+
+    enum class Sym {
+        notype,
+        object,
+        func,
+        section,
+        file,
+        NULL_SYM
+    };
+    
+    struct Symbol {
+        Sym s;
+        std::string name;
+        uintptr_t addr;
+    };
+
+    std::string getNameFromSym(Sym s) const;
+    Sym getSymFromElf(elf::stt s) const;
+
+    const std::vector<Symbol>& getSymbolListFromName(const std::string& name);
+    
+private:
+    const elf::elf elf_;
+    std::unordered_map<std::string, std::vector<Symbol>> symbolCache;
+
+};
---
+++ src/symbolmap.cpp
@@ -0,0 +1,74 @@
+#include "../include/symbolmap.h"
+
+#include <elf/elf++.hh> 
+
+#include <bit>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <cstdint>
+
+
+SymbolMap::SymbolMap() : elf_() {}
+SymbolMap::SymbolMap(const elf::elf& elf) : elf_(elf) {}
+
+std::string SymbolMap::getNameFromSym(SymbolMap::Sym s) const {
+    switch(s) {
+        case Sym::notype:
+            return "notype";
+        case Sym::object:
+            return "object";
+        case Sym::func:
+            return "func";
+        case Sym::section:
+            return "section";
+        case Sym::file:
+            return "file";
+    }
+    return "NULL_SYM";
+}
+
+SymbolMap::Sym SymbolMap::getSymFromElf(elf::stt s) const {
+    switch(s) {
+        case elf::stt::notype:
+            return Sym::notype;
+        case elf::stt::object:
+            return Sym::object;
+        case elf::stt::func:
+            return Sym::func;
+        case elf::stt::section:
+            return Sym::section;
+        case elf::stt::file:
+            return Sym::file;
+    }
+    return Sym::NULL_SYM;
+}
+
+const std::vector<SymbolMap::Symbol>& SymbolMap::getSymbolListFromName(const std::string& name) {
+    auto it = symbolCache.find(name);
+    if(it != symbolCache.end()) return it->second;
+
+    std::vector<Symbol> v;
+
+    for(auto& section : elf_.sections()) {
+        auto type = section.get_hdr().type;
+        if(type != elf::sht::symtab || type != elf::sht::dynsym) continue;
+
+        for(auto symbol : section.as_symtab()) {
+            if(symbol.get_name() == name) {
+                auto& symData = symbol.get_data();
+                auto symElfType = getSymFromElf(symData.type());
+
+                v.emplace_back(Symbol(symElfType, getNameFromSym(symElfType), 
+                    std::bit_cast<uintptr_t>(symData.value)));
+            }
+        }
+    }
+
+    symbolCache[name] = v;
+    it = symbolCache.find(name);
+    return it->second;
+    
+}
+
+
